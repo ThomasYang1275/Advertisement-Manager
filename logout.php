337e1ae55c5964d9c4ce403543cae8176dbfd676
Thomas Yang <smartcow831@gmail.com>
2021-04-26 02:27:23 -0500

    Add files via upload
    Add style, ads, and logout page. update home

---
+++ logout.php
@@ -0,0 +1,16 @@
+<!-- logout code from: https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php -->
+
+<?php
+// Initialize the session
+session_start();
+ 
+// Unset all of the session variables
+$_SESSION = array();
+ 
+// Destroy the session.
+session_destroy();
+ 
+// Redirect to login page
+header("location: login.php");
+exit;
+?>
